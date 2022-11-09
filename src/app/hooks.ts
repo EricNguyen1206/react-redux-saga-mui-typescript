e8318421307599e0e23e58cd36ec874e6e5f7125
EricNgGeekup <tin.nt@geekup.vn>
2022-11-09 16:06:28 +0700

    Initial project base

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,3 +1,8 @@
+/** file overview
+ *  @useAppDispatch to replace useDispatch, redux implicite typing for dispatch is AppDispatch, and midleware apply able
+ *  @useAppSelector to replace useSelector, you donnot have to define type, redux implicite understand the state you remind is rootState
+ */
+
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from './store';
 
